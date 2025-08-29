0cc5617cb515191928e317288e26a34c820b888d
SylviaRemington <sylviaremington@gmail.com>
2025-08-29 15:39:00 -0700

    passing funmoments data into FunMomentList

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
         <Route path='/sign-in' element={<SignInForm />} />
         {/* Protected Route below /funmoments that is only available if you are signed in as a user. */}
         {/* Need to pass the FunMoments data in into the FunMomentList. */}
-        <Route path='funmoments' element={<FunMomentList />}></Route>
+        <Route path='funmoments' element={<FunMomentList funmoments={funmoments} />}></Route>
       </Routes>
     </>
   );
