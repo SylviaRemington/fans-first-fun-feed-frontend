7769c629f1b4b2d796e20217d6f458da7582be91
SylviaRemington <sylviaremington@gmail.com>
2025-08-29 13:36:21 -0700

    building the service function for the funmomentService

--- src/services/funmomentService.js
+++ src/services/funmomentService.js
@@ -1,2 +1,6 @@
 // Creating funmomentService.js - creating the service that will talk to the API
+// This is my base url for my funmomentService.js
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/funmoments`;
+
+
+// Building the service function
