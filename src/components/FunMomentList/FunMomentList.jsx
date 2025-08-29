8c169e36a5df0c0ed6784dcfa18d3e75b6650668
SylviaRemington <sylviaremington@gmail.com>
2025-08-28 21:57:08 -0700

    addtl notes about creating FunMomentList component & importing FunMomentList into App.jsx & creating a Route path & element

--- src/components/FunMomentList/FunMomentList.jsx
+++ src/components/FunMomentList/FunMomentList.jsx
@@ -1,5 +1,9 @@
+// Starting with simple function to check to make sure everything is working
 const FunMomentList = (props) => {
   return <main>Fun Moment List</main>;
 };
 
 export default FunMomentList;
+
+
+// checked and my component is working after creating this, importing into App.jsx & then creating a Route path with an element.
