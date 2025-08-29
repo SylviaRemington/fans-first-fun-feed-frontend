35b9b11e789d1af5252830141ef3ff6dc745c139
SylviaRemington <sylviaremington@gmail.com>
2025-08-29 14:35:01 -0700

    adding axios & using axios instead of fetch in the code for more ease for funmomentService.js

--- src/services/funmomentService.js
+++ src/services/funmomentService.js
@@ -1,6 +1,28 @@
+// path is src/services/funmomentService.js
+
+// Imports
+import axios from "axios";
+
 // Creating funmomentService.js - creating the service that will talk to the API
 // This is my base url for my funmomentService.js
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/funmoments`;
 
 
-// Building the service function
+// Building the service function - building out the index () functionality
+// Making a request to /funmoments so no modification to BASE_URL is necessary.
+// Using axios instead of fetch for more ease
+const index = async () => {
+  try {
+    const res = await axios.get(BASE_URL, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+    
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { 
+  index,
+};
