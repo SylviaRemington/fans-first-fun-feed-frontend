aa35ec60124d6e64c96549d9fb6c788805294338
SylviaRemington <sylviaremington@gmail.com>
2025-08-30 10:22:51 -0700

    correcting bug for FunMomentDetails

--- src/components/FunMomentDetails/FunMomentDetails.jsx
+++ src/components/FunMomentDetails/FunMomentDetails.jsx
@@ -1,15 +1,25 @@
-import { useParams, useEffect } from "react-router";
+import { useEffect } from "react";
+import { useParams } from "react-router";
 import * as funmomentService from '../../services/funmomentService';
 
 const FunMomentDetails = () => {
-    const params = useParams;
-    console.log(params.id);
+    const { id } = useParams;
+    console.log(id);
+
+    // Original Version before the above change:
+    // const params = useParams; - Wrong: This assigns the useParams function itself, not its result.
+    // console.log(params.id); - This logs undefined because params is the function, not an object.
+
 
     useEffect(() => {
-        const funmomentToShow = await funmomentService.show()
-    })
+        const getData = async () => {
+            const funmomentToShow = await funmomentService.show(params.id);
+            console.log(funmomentToShow);
+        };
+        getData();
+    }, []);
 
-  return <main>Fun Moment Details</main>;
+    return <main>Fun Moment Details</main>;
 };
 
 export default FunMomentDetails;
