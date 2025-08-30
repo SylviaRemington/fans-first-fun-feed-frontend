8589f3497d36c56c25368d20c537b938a866b23b
SylviaRemington <sylviaremington@gmail.com>
2025-08-30 09:15:19 -0700

    adding route path and element for FunMomentDetails

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import FunMomentList from './components/FunMomentList/FunMomentList';
+import FunMomentDetails from './components/FunMomentDetails/FunMomentDetails';
 
 import { UserContext } from './contexts/UserContext';
 
@@ -41,7 +42,8 @@ const App = () => {
         <Route path='/sign-in' element={<SignInForm />} />
         {/* Protected Route below /funmoments that is only available if you are signed in as a user. */}
         {/* Need to pass the FunMoments data in into the FunMomentList component. */}
-        <Route path='funmoments' element={<FunMomentList funmoments={funmoments} />}></Route>
+        <Route path='funmoments' element={<FunMomentList funmoments={funmoments} />} />
+        <Route path="/funmoments/:id" element={<FunMomentDetails/>} />
       </Routes>
     </>
   );
--- src/components/FunMomentDetails/FunMomentDetails.jsx
+++ src/components/FunMomentDetails/FunMomentDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 const FunMomentDetails = () => {
     const params = useParams;
     console.log(params);
-    
+
   return <main>Fun Moment Details</main>;
 };
 
