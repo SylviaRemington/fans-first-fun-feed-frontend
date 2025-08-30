5006eec5e6c6b9581751b093a6404cb591862c49
SylviaRemington <sylviaremington@gmail.com>
2025-08-30 10:36:50 -0700

    addtl

--- src/components/FunMomentDetails/FunMomentDetails.jsx
+++ src/components/FunMomentDetails/FunMomentDetails.jsx
@@ -13,6 +13,7 @@ const FunMomentDetails = () => {
     // console.log(id);
 
     useEffect(() => {
+        // Setting up our data fetching function & call the show function with that id (because the funmomentService needs the id for the show function)
         const getData = async () => {
             const funmomentToShow = await funmomentService.show(params.id);
             console.log(funmomentToShow);
