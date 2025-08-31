008c3a2db7dc5a7a7a120b648e3a8f94baf261ce
SylviaRemington <sylviaremington@gmail.com>
2025-08-31 01:01:28 -0700

    added new handle function however I think I'm going to take that out and do something different

--- src/components/FunMomentForm/FunMomentForm.jsx
+++ src/components/FunMomentForm/FunMomentForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import * as funmomentService from "../../services/funmomentService";
 
 // When first start the code, using a super simple function to make sure code and route is working to browser.
@@ -9,9 +9,9 @@ import * as funmomentService from "../../services/funmomentService";
 
 const FunMomentForm = (props) => {
   const [formData, setFormData] = useState({
-    title: '',
-    text: '',
-    category: 'Trick Plays',
+    title: "",
+    text: "",
+    category: "Trick Plays",
   });
 
   //handleChange function
@@ -20,66 +20,64 @@ const FunMomentForm = (props) => {
   };
 
   //New addtl handle function here...
-
-
+  const handleAddFunMoment = async () => {
+    try {
+      const newFunMoment = await funmomentService.create(formData);
+      console.log("SUCCESS", newFunMoment);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   //handleSubmit function
   const handleSubmit = async (evt) => {
     evt.preventDefault();
-    console.log('formData', formData);
-
-    try {
-        const newFunMoment = await funmomentService.create(formData)
-        console.log('SUCCESS', newFunMoment)
-    } catch (error) {
-        console.log(error);
-    }
-
+    console.log("formData", formData);
   };
 
   return (
     <main>
       <form onSubmit={handleSubmit}>
-        <label htmlFor='title-input'>Title</label>
+        <label htmlFor="title-input">Title</label>
         <input
           required
-          type='text'
-          name='title'
-          id='title-input'
+          type="text"
+          name="title"
+          id="title-input"
           value={formData.title}
           onChange={handleChange}
         />
-        <label htmlFor='text-input'>Text</label>
+        <label htmlFor="text-input">Text</label>
         <textarea
           required
-          type='text'
-          name='text'
-          id='text-input'
+          type="text"
+          name="text"
+          id="text-input"
           value={formData.text}
           onChange={handleChange}
         />
-        <label htmlFor='category-input'>Category</label>
+        <label htmlFor="category-input">Category</label>
 
         {/* This below handles the enum for the category between the select tags. */}
         <select
           required
-          name='category'
-          id='category-input'
+          name="category"
+          id="category-input"
           value={formData.category}
           onChange={handleChange}
         >
-          <option value='Trick Plays'>Trick Plays</option>
-          <option value='Cool Dances'>Cool Dances</option>
-          <option value='Songs Sung'>Songs Sung</option>
-          <option value='Individual Players'>Individual Players</option>
-          <option value='Dad Bod Squad'>Dad Bod Squad</option>
-          <option value='Princess'>Princess</option>
-          <option value='Umpire Moves'>Umpire Moves</option>
-          <option value='Mascot'>Mascot</option>
-          <option value='Funny Audience Games'>Funny Audience Games</option>
+          <option value="Trick Plays">Trick Plays</option>
+          <option value="Cool Dances">Cool Dances</option>
+          <option value="Songs Sung">Songs Sung</option>
+          <option value="Individual Players">Individual Players</option>
+          <option value="Dad Bod Squad">Dad Bod Squad</option>
+          <option value="Princess">Princess</option>
+          <option value="Umpire Moves">Umpire Moves</option>
+          <option value="Mascot">Mascot</option>
+          <option value="Funny Audience Games">Funny Audience Games</option>
         </select>
 
-        <button type='submit'>SUBMIT</button>
+        <button type="submit">SUBMIT</button>
       </form>
     </main>
   );
