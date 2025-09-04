610cb2fe10b967a7a8ca8d66562e49013121dce7
SylviaRemington <sylviaremington@gmail.com>
2025-09-04 11:44:42 -0700

    updating landing page with new verbiage that works better

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,14 @@
 const Landing = () => {
   return (
     <main>
-      <h1>Hello, you are on the landing page for visitors.</h1>
-      <p>Sign up now, or sign in to see your super secret dashboard!</p>
+      <h1>Welcome to the Fans First Fun Feed!</h1>
+      <h2>This is an app for fans to share great, fun moments from Savannah Bananas games!</h2>
+      <h2>...And relive the magic of the games!</h2>
+
+      <p>SIGN UP or SIGN IN (above) to see your SUPER AWESOME Fans First Feed!</p>
     </main>
   );
 };
 
 export default Landing;
+
